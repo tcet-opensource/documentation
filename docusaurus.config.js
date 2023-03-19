5f4be3cc0b3d3d877f3a42c11e91239f5475a0cc
demonkiller <gaurangvishwakarma619@gmail.com>
2023-03-19 10:04:55 +0530

    fix silly error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tcet-opensource', // Usually your GitHub org/user name.
-  projectName: 'documenation', // Usually your repo name.
+  projectName: 'documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
