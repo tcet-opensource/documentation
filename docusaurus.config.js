77243d36fd2f1b3fe85a30bf07dd93d58c23d25d
demonkiller <gaurangvishwakarma619@gmail.com>
2023-03-19 10:10:48 +0530

    update title in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'TCET Open Source Docs',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
