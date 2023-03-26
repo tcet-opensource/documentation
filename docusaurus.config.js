e2304d5849dc4e94dca9bb92e6fd7348a8137a49
demonkiller <gaurangvishwakarma619@gmail.com>
2023-03-26 15:34:30 +0530

    Revert "update title in config"
    This reverts commit 77243d36fd2f1b3fe85a30bf07dd93d58c23d25d.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'TCET Open Source Docs',
+        title: 'My Site',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
