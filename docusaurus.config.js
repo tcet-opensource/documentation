2aebabbbbaf13d930f07c997b08f1bbcba83518e
demonkiller <gaurangvishwakarma619@gmail.com>
2023-04-01 03:34:09 +0530

    update config header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Project Docs',
+  title: 'Project Documentation',
   tagline: 'Documentation of all TCET Open Source Projects',
   favicon: 'img/favicon.ico?v=3',
 
