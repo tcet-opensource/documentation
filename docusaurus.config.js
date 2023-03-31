c5a40a058ec0ee5cbd03ccebeaa0eb93e7e714fa
demonkiller <gaurangvishwakarma619@gmail.com>
2023-04-01 03:09:58 +0530

    update config base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://tcet-opensource.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/documentation',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
