db2a661bb927df7a6204ee42f65e0fe1a2ad200c
demonkiller <gaurangvishwakarma619@gmail.com>
2023-04-01 03:49:46 +0530

    update description

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="TCET Open Source Website & Projects Documentation">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
