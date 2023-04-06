3324be3d51ad55a0d56392c30c4e1c8b85d0b4a2
vish-rt <rawatvishal1707@gmail.com>
2023-04-07 00:27:14 +0530

    renamed intro.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,8 +91,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'About',
+                to: '/docs/about',
               },
             ],
           },
