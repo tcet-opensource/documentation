e7996088439f762e8488cb17ca8d30fa57f0b5f3
demonkiller <gaurangvishwakarma619@gmail.com>
2023-04-09 12:47:31 +0530

    [FEATURE] dynamic and modular sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'docs',
             position: 'left',
             label: 'Docs',
           },
