fe06fb7a810768854c280535aa90ec3c0da66b68
demonkiller <gaurangvishwakarma619@gmail.com>
2023-04-13 13:18:49 +0530

    [FIX] import head

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
 
 import styles from './index.module.css';
 
