6dda7106aa99452ea9325d9bba9f3dc9b9f7f4e0
Vansh-Baghel <vanshbaghel912@gmail.com>
2023-04-30 10:33:07 +0530

    Made responsive and completed

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,25 +6,22 @@ import styles from "./index.module.css";
 
 const About = () => {
   return (
-    <div
-      className={clsx(
-        "",
-        styles.aboutBackground
-      )}
-    >
-      <div
-        className=
-          "flex justify-center items-center w-[90%] m-auto"
-      >
-        <div className='flex items-center gap-12 px-16 py-20 2xl:px-32 lg:py-40'>
-          <div className='w-[50%]'>
+    <div className={clsx("", styles.aboutBackground)}>
+      <div className='flex justify-center items-center w-[90%] m-auto'>
+        <div className='flex flex-col items-center gap-12 px-16 py-20 max-sm:px-0 max-sm:py-16 lg:flex-row 2xl:px-32 lg:py-40'>
+          <div>
             <img src={img} alt='img' />
           </div>
-          <div className='flex flex-col items-start gap-6 w-[50%]'>
-            <div className={clsx("text-5xl", styles.fontSora)}>
+          <div className='flex flex-col items-start gap-6 lg:w-1/2'>
+            <div
+              className={clsx(
+                "text-4xl text-center w-full max-xs:text-[2rem] lg:text-start lg:w-[110%]",
+                styles.fontSora
+              )}
+            >
               TCET-OPEN SOURCE
             </div>
-            <div className='text-xl leading-7 text-[#D0D5DD]'>
+            <div className='text-xl leading-7 max-xs:text-sm text-[#D0D5DD]'>
               In the present scenario of globalization there is tremendous
               demand for skilled manpower particularly in the area of
               Engineering and Technology. Therefore, Training and Placement
@@ -38,11 +35,11 @@ const About = () => {
                   <CountUp
                     start={0}
                     end={852}
-                    className='text-[28px] leading-9'
+                    className='text-[28px] leading-9 max-xs:text-lg'
                   />
                 </div>
                 <div>
-                  <div className='#D0D5DD text-md'>Number of Line code</div>
+                  <div className='#D0D5DD text-md max-xs:text-sm'>Number of Line code</div>
                 </div>
               </div>
               <div className='flex flex-col items-start p-3 w-[45%] gap-2 bg-[#302261] rounded-xl'>
@@ -51,11 +48,11 @@ const About = () => {
                   <CountUp
                     start={0}
                     end={7400}
-                    className='text-[28px] leading-9'
+                    className='text-[28px] leading-9 max-xs:text-lg'
                   />
                 </div>
                 <div>
-                  <div className='#D0D5DD text-md'>Number of Line code</div>
+                  <div className='#D0D5DD text-md max-xs:text-sm'>Number of Line code</div>
                 </div>
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+      screens: {
+      'xs': '440px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      }
   },
   plugins: [],
 }
