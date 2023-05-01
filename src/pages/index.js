c2833d72bf998211aa4ac96ac3aa4f83626ba0b7
Riya Vishwakarma <riyavishwa2001@gmail.com>
2023-05-01 17:31:19 +0530

    Added Sora and Inter Fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,22 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
 import ProjectSection from "../components/ProjectSection";
 
+const link = document.createElement('link');
+link.rel = 'preconnect';
+link.href = 'https://fonts.googleapis.com';
+document.head.appendChild(link);
+
+const link2 = document.createElement('link');
+link2.rel = 'preconnect';
+link2.href = 'https://fonts.gstatic.com';
+link2.crossOrigin = 'true';
+document.head.appendChild(link2);
+
+const link3 = document.createElement('link');
+link3.href = 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Sora:wght@600&display=swap';
+link3.rel = 'stylesheet';
+document.head.appendChild(link3);
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        title: ["Sora", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
+
 
