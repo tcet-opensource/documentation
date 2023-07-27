4362edd0c6a86c3e215568dda8bdaf0604df7dcb
demonkiller <gaurangvishwakarma619@gmail.com>
2023-07-27 23:47:40 +0530

    [DEL] openconf info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,14 +81,14 @@ const config = {
       },
       metadata: [{name: 'TCET Open Source', content: 'opensource, software'}],
       // Do not remove announcement bar. Comment it when not required.
-      announcementBar: {
-        id: 'openconf23',
-        content:
-          'OpenConf 2023 is on July  21st!!  <a target="_blank" rel="noopener noreferrer" href="https://lu.ma/openconf">Register for the event.</a>',
-        backgroundColor: '#fafbfc',
-        textColor: '#091E42',
-        isCloseable: true,
-      },
+      // announcementBar: {
+      //   id: 'openconf23',
+      //   content:
+      //     'OpenConf 2023 is on July  21st!!  <a target="_blank" rel="noopener noreferrer" href="https://lu.ma/openconf">Register for the event.</a>',
+      //   backgroundColor: '#fafbfc',
+      //   textColor: '#091E42',
+      //   isCloseable: true,
+      // },
       navbar: {
         hideOnScroll: true,
         title: 'TCET Open Source',
@@ -105,11 +105,11 @@ const config = {
             label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            label: 'OpenConf 2023',
-            href: 'https://openconf.tcetmumbai.in',
-            position: 'left',
-          },
+          // {
+          //   label: 'OpenConf 2023',
+          //   href: 'https://openconf.tcetmumbai.in',
+          //   position: 'left',
+          // },
           {
             href: 'https://github.com/tcet-opensource/documentation',
             label: 'GitHub',
