4bce9704312815b11a3e1d1b345db5b9dd6cf9fa
Shlok Prajapati <shlokprajapati2003@gmail.com>
2023-09-06 22:29:40 +0530

    Updated Project Overlaying text

--- src/components/ProjectSection/ProjectPreview.js
+++ src/components/ProjectSection/ProjectPreview.js
@@ -2,22 +2,29 @@ import React from "react";
 
 const ProjectPreview = ({ img, tools }) => {
   return (
-    <div className="relative flex object-cover h-[90%] text-white md:px-12">
-      <img
-        src={"/img/ProjectSection/" + img}
+    <div className="relative object-cover px-10 h-[90%] text-white md:px-12 overflow-hidden">
+      <img  
+        src={`/img/ProjectSection/${img}`}
         alt="project-img"
-        className="object-cover rounded-none md:rounded-3xl"
+        className="object-cover overflow-hidden rounded-none md:rounded-3xl"
       />
-      <div className="absolute hidden md:block bottom-5 left-5 md:bottom-14 md:left-28">
-        <div className="flex justify-start pl-2 font-semibold md:text-2xl">
-          TOOL:
+        <div 
+          className="absolute rounded-md px-2 text-left hidden md:block bottom-0 left-5 md:bottom-14 md:left-28 outline-none"
+          style={{
+            "--bg-opacity": 0.2,
+            "backdrop-filter": "blur(7px)",
+            "background": "linear-gradient(to top, rgb(17 24 39 / var(--bg-opacity)), transparent), linear-gradient(to right, rgb(17 24 39 / var(--bg-opacity)), transparent 70%)",
+          }}
+        >
+          <div className="flex justify-start pl-2 font-semibold md:text-2xl">
+            TOOL:
+          </div>
+          {tools.map(tool =>
+            <span className="px-2 font-light md:text-2xl">
+              {tool + " "}
+            </span>
+          )}
         </div>
-        {tools.map(tool =>
-          <sapn className="px-2 font-light md:text-2xl">
-            {tool + " "}
-          </sapn>
-        )}
-      </div>
     </div>
   );
 };
