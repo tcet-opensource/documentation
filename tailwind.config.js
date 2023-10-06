4ab1ef544a2d263a69d131843b702c4c72361fb2
enayush <en.ayushshr@gmail.com>
2023-10-06 22:09:17 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ module.exports = {
         title: ["Sora", ...defaultTheme.fontFamily.serif],
       },
     },
+    fontSize: {
+      '5xl': '2.8rem',
+      '6xl': '3.75rem',
+      '7xl': '4.5rem',
+    },
   },
   corePlugins: {
     preflight: false,
